8e2621f8b1a154aac1be8771a4a36863ed930ec1
Philve <47560937+philve@users.noreply.github.com>
2023-03-28 19:05:32 +0330

    Update Project.ts

--- app/dashboard/src/constants/Project.ts
+++ app/dashboard/src/constants/Project.ts
@@ -1,3 +1,3 @@
 export const REPO_URL = "https://t.me/FiNet_Channel";
 export const ORGANIZATION_URL = "https://t.me/Fi_Net";
-export const DONATION_URL = "";
+export const DONATION_URL = "https://t.me/FiNet_Channel";
